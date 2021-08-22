d1ce6652699d3908187da991bd5281c17008e283
Lindsey JJ <ljjlauria@mac.com>
2021-08-22 14:35:36 -0700

    cities populating dropdown and updating state hook

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import { Container, Table, Col, Row, Button } from 'react-bootstrap'
 import { Dropdown } from 'semantic-ui-react';
-import { useState } from 'react';
+import { useState} from 'react';
 import { useQuery } from '@apollo/client';
 import { QUERY_ME } from '../utils/queries';
 
 const CityTable = () => {
 
-    const [cityValue, setCityValue] = useState();
+    const [cityValue, setCityValue] = useState([]);
+
+    const handleChange = (e, {value}) => {
+        setCityValue(value);
+        console.log(value);
+    }
 
     const { loading, data } = useQuery(QUERY_ME);
+
     const userData = data?.me || [];
 
       // if data isn't here yet, say so
@@ -18,100 +24,77 @@ const CityTable = () => {
     }
 
     return (
+
         <Container>
             <Row className="p-3">
-
-                <Col>         
-                    
-
-
-                </Col>
-                
+                <Col></Col>       
             </Row>
 
 
              <h2>
             {userData.savedCities?.length
-            ? `Viewing ${userData.savedCities.length} saved ${userData.savedCities.length === 1 ? 'city' : 'cities'}.`
+            ? `You have ${userData.savedCities.length} saved ${userData.savedCities.length === 1 ? 'city' : 'cities'} to compare.`
             : 'You have no saved cities.'}
             </h2>
 
+            <Dropdown className="mb-10" placeholder='City to Compare' onChange={handleChange} fluid multiple selection options={
+                userData.savedCities?.map((city) => {  
+                    return {
+                        key: city._id,
+                        value: city._id,
+                        text: city.name,
+                        disabled: cityValue.length > 1 ? true : false
+                    }
+                })
+            } />
 
-            {userData.savedCities?.map((city) => {
-                return (
-                    <h2 key={city._id}>{city._id}</h2>
-                );
-             })}   
-{/* 
-            <Dropdown onChange={async (e, { name , value }) => {setCityValue(value)}}
-            options={userData.savedCities.map((currentValue, index) => {
-            return {
-                key: `${currentValue.name}`,
-                value: `${currentValue.name}`,
-                text: `${currentValue.name}`,
-                disabled: cityValue.length > 2 ? true : false
 
-            }
-            /> */}
-            
-            
-
-            <Table striped bordered hover>
+            {/* <Table striped bordered hover>
                 <thead>
                     <tr>
                         <th></th>
-                        <th>State 1</th>
-                        <th>State 2</th>
+                        <th>${city.name}</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <td>Population</td>
-                        <td></td>
-                        <td></td>
+                        <td>${city.population}</td>
                     </tr>
                    <tr>
                         <td>Healthcare</td>
-                        <td></td>
-                        <td></td>
+                        <td>${city.population}</td>
                     </tr>
                     <tr>
                         <td>Taxation</td>
-                        <td></td>
-                        <td></td>
+                        <td>${city.taxation}</td>
                     </tr>
                     <tr>
                         <td>Education</td>
-                        <td></td>
-                        <td></td>
+                        <td>${city.education}</td>
                     </tr>
                     <tr>
                         <td>Housing</td>
-                        <td></td>
-                        <td></td>
+                       <td>${city.population}</td>
                     </tr>
                     <tr>
                         <td>Living</td>
-                        <td></td>
-                        <td></td>
+                        <td>${city.costOfLiving}</td>
                     </tr>
                      <tr>
                         <td>Safety</td>
-                        <td></td>
-                        <td></td>
+                        <td>${city.safety}</td>
                     </tr>
                      <tr>
                         <td>Environment</td>
-                        <td></td>
-                        <td></td>
+                        <td>${city.environment}</td>
                     </tr> 
                     <tr>
                         <td>Economy</td>
-                        <td></td>
-                        <td></td>
+                        <td>${city.economy}</td>
                     </tr>
                 </tbody>
-            </Table>
+            </Table> */}
 
 
 
